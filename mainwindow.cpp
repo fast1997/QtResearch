316250be2ff030493938621a88cec183a9831ad6
Andy Dang <andydang456@yahoo.com>
2018-10-16 19:49:25 -0700

    clearer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,6 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
 {
     ui->setupUi(this);
     setMouseTracking(true);
-<<<<<<< HEAD
     scene = new QGraphicsScene();
     view = ui->graphicsView;
     view->setScene(scene);
@@ -28,26 +27,6 @@ void MainWindow::paintEvent(QPaintEvent * event)
 
 }
 
-=======
-    scene = new QGraphicsScene(this);
-    othello = new Game(scene,this);
-    othello->Display();
-    ui->graphicsView->setScene(scene);
-
-}
-
-void MainWindow::mousePressEvent(QMouseEvent *event){
-    if(event->buttons() == Qt::LeftButton)
-    {
-        othello->Click(event->pos());
-    }
-}
-void MainWindow::paintEvent(QPaintEvent * event)
-{
-    qDebug() << "Hello";
-    othello->Display();
-}
->>>>>>> e1b351f58c7540dcd016904f890fae707e6f0ca0
 MainWindow::~MainWindow()
 {
     delete ui;
